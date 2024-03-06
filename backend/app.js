d18a37eafe2c727435e9f4a0f55f1c5dc11519d4
de Breyne <111784301+helio75011@users.noreply.github.com>
2024-03-06 11:30:13 +0100

    create test API

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/test', (_, res) => {
+    res.send({
+        msg: 'Hello les amis !'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`port : ${PORT}`);
+})
